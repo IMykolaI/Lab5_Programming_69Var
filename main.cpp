b7d408703ddfcd73a1ae777053603bd22d89e5a5
Mykola Motychak <koliamotychak@gmail.com>
2018-11-14 22:34:39 +0200

    Updating code in the main.cpp file into clean code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 
-#include "footwear.h"
+#include "shoes.h"
 #include <stdio.h>
 #include <iostream>
 #include <string>
@@ -9,40 +9,40 @@ using namespace std;
 int main()
 {
 	Footwear standart;
-	cout << "Brand: " << standart.getName() << endl;
-	cout << "Model: " << standart.getModel() << endl;
-	cout << "Gender: " << standart.getGender() << endl;
-	cout << "Matherial: " << standart.getMatherial() << endl;
-	cout << "Size: " << standart.getSize() << endl;
-	cout << "Color: " << standart.getColor() << endl;
-	cout << "Price: " << standart.getPrice() << "$ (" << standart.getDiscount() << "% OFF)\n" << endl;
+	cout << "Brand: " << standart.getBrandName() << endl;
+	cout << "Model: " << standart.getModelOfFootwear() << endl;
+	cout << "Gender: " << standart.getGenderOfBuyer() << endl;
+	cout << "Matherial: " << standart.getMatherialOfFootwear() << endl;
+	cout << "Size: " << standart.getSizeOfFootwear() << endl;
+	cout << "Color: " << standart.getColorOfFootwear() << endl;
+	cout << "Price: " << standart.getPriceOfFootwear() << "$ (" << standart.getDiscountOnFootwear() << "% OFF)\n" << endl;
 
 	Footwear Nike("Nike", "Downshifter 7", "For Men", "Textile/Phylon(Rubber)", "6 - 15(Step - 0.5)", "Black/Volt/White", 50.25, 16);
-	cout << "Brand: " << Nike.getName() << endl;
-	cout << "Model: " << Nike.getModel() << endl;
-	cout << "Gender: " << Nike.getGender() << endl;
-	cout << "Matherial: " << Nike.getMatherial() << endl;
-	cout << "Size: " << Nike.getSize() << endl;
-	cout << "Color: " << Nike.getColor() << endl;
-	cout << "Price: " << Nike.getPrice() << "$ (" << Nike.getDiscount() << "% OFF)\n" << endl;
+	cout << "Brand: " << Nike.getBrandName() << endl;
+	cout << "Model: " << Nike.getModelOfFootwear() << endl;
+	cout << "Gender: " << Nike.getGenderOfBuyer() << endl;
+	cout << "Matherial: " << Nike.getMatherialOfFootwear() << endl;
+	cout << "Size: " << Nike.getSizeOfFootwear() << endl;
+	cout << "Color: " << Nike.getColorOfFootwear() << endl;
+	cout << "Price: " << Nike.getPriceOfFootwear() << "$ (" << Nike.getDiscountOnFootwear() << "% OFF)\n" << endl;
 
 	Footwear Asics("Asics", "GT-2000 5", "For Women", "Textile/Rubber", "5 - 11.5(Step - 0.5)", "Midgrey/White/Pink Glow", 74.95, 38);
-	cout << "Brand: " << Asics.getName() << endl;
-	cout << "Model: " << Asics.getModel() << endl;
-	cout << "Gender: " << Asics.getGender() << endl;
-	cout << "Matherial: " << Asics.getMatherial() << endl;
-	cout << "Size: " << Asics.getSize() << endl;
-	cout << "Color: " << Asics.getColor() << endl;
-	cout << "Price: " << Asics.getPrice() << "$ (" << Asics.getDiscount() << "% OFF)\n" << endl;
+	cout << "Brand: " << Asics.getBrandName() << endl;
+	cout << "Model: " << Asics.getModelOfFootwear() << endl;
+	cout << "Gender: " << Asics.getGenderOfBuyer() << endl;
+	cout << "Matherial: " << Asics.getMatherialOfFootwear() << endl;
+	cout << "Size: " << Asics.getSizeOfFootwear() << endl;
+	cout << "Color: " << Asics.getColorOfFootwear() << endl;
+	cout << "Price: " << Asics.getPriceOfFootwear() << "$ (" << Asics.getDiscountOnFootwear() << "% OFF)\n" << endl;
 
 	Footwear NewBalance("New Balance", "M990v3", "For Men/Women", "Textile/Rubber", "16", "Grey", 149.95, 0);
-	cout << "Brand: " << NewBalance.getName() << endl;
-	cout << "Model: " << NewBalance.getModel() << endl;
-	cout << "Gender: " << NewBalance.getGender() << endl;
-	cout << "Matherial: " << NewBalance.getMatherial() << endl;
-	cout << "Size: " << NewBalance.getSize() << endl;
-	cout << "Color: " << NewBalance.getColor() << endl;
-	cout << "Price: " << NewBalance.getPrice() << "$ (" << NewBalance.getDiscount() << "% OFF)\n" << endl;
+	cout << "Brand: " << NewBalance.getBrandName() << endl;
+	cout << "Model: " << NewBalance.getModelOfFootwear() << endl;
+	cout << "Gender: " << NewBalance.getGenderOfBuyer() << endl;
+	cout << "Matherial: " << NewBalance.getMatherialOfFootwear() << endl;
+	cout << "Size: " << NewBalance.getSizeOfFootwear() << endl;
+	cout << "Color: " << NewBalance.getColorOfFootwear() << endl;
+	cout << "Price: " << NewBalance.getPriceOfFootwear() << "$ (" << NewBalance.getDiscountOnFootwear() << "% OFF)\n" << endl;
 
 	system("pause");
 }
